e852703c71aba63af18bccdf3edc51a6c8989b30
Sergei Anipchenko <sergei_anip20@mail.ru>
2022-12-21 14:22:25 +0300

    dz3

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -42,23 +42,23 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
-
-        onEnter?.()
         setEditMode(false)
+        onEnter?.()
+
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
-        onBlur?.(e)
         setEditMode(false)
+        onBlur?.(e)
+
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
-        onDoubleClick?.(e)
         setEditMode(true)
+        onDoubleClick?.(e)
+
     }
 
     const spanClassName = s.span
