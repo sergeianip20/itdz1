3bc41b158be198fc332d0c476f60b27dd66e8a6b
Sergei Anipchenko <sergei_anip20@mail.ru>
2023-01-07 12:12:50 +0300

    dz8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,40 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+        if(action.payload=== 'up'){
+
+
+            state.sort((a:any,b:any)=> {
+                if (a.name > b.name) {
+                    return 1
+                }
+                if (a.name < b.name) {
+                    return -1
+                }
+                return 0
+             })
+
+        }
+        if(action.payload === 'down'){
+            state.sort((a:any, b:any)=>{
+                if(a.name < b.name){
+                    return 1
+                }
+                if(a.name > b.name){
+                    return -1
+                }
+                return 0
+            })
+        }
+            return [...state] // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+ let filterstate =  state.filter((e)=> e.age >= 18)
+            return filterstate// need to fix
         }
         default:
             return state
