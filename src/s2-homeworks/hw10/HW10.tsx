edd7e48faefb530e222c1b55013b35ff205acc2b
Sergei Anipchenko <sergei_anip20@mail.ru>
2023-01-19 21:21:34 +0300

    dz9

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {initStateType, loadingAC, LoadingActionType} from './bll/loadingReducer'
+import {initStateType, loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -28,6 +28,7 @@ const HW10 = () => {
             dispatch(loadingAC(false))
         }, 1500)
     }
+
 
     return (
         <div id={'hw10'}>
